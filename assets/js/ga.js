2d7a936b7466036e3009696dff03ad709c755c72
spike1292 <spike1292@gmail.com>
2018-04-13 22:35:24 +0200

    update to analytics.js

---
+++ assets/js/ga.js
@@ -0,0 +1,8 @@
+window.ga =
+    window.ga ||
+    function() {
+        (ga.q = ga.q || []).push(arguments);
+    };
+ga.l = +new Date();
+ga("create", "UA-8856702-6", "auto");
+ga("send", "pageview");
