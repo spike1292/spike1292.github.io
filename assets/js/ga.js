8cf9c9f9c0a2dd81dab2fd71deeb70f17051a18a
Henk Bakker <spike1292@gmail.com>
2019-11-24 19:10:45 +0100

    upgrade to gtag

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,10 +1,8 @@
-window.ga =
-    window.ga ||
-    function() {
-        (ga.q = ga.q || []).push(arguments);
-    };
-ga.l = +new Date();
-
-ga("create", "UA-8856702-6", "auto");
-ga("set", "cd1", "online");
-ga("send", "pageview");
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "{{ site.analytics-google }}", {
+    dimension1: "online"
+});
