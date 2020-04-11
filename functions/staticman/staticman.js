18a565c1c27425ef80d347e8e23b191c2bef4fbb
Henk Bakker <spike1292@gmail.com>
2020-04-11 21:17:51 +0200

    make staticman async

--- functions/staticman/staticman.js
+++ functions/staticman/staticman.js
@@ -14,23 +14,24 @@ exports.handler = (event, context, callback) => {
         version: "2",
     };
 
-    const staticman = new Staticman(params);
-    staticman.setConfigPath();
-    staticman.setIp(event.headers["client-ip"]);
-    staticman.setUserAgent(event.headers["user-agent"]);
+    new Staticman(params).then((staticman) => {
+        staticman.setConfigPath();
+        staticman.setIp(event.headers["client-ip"]);
+        staticman.setUserAgent(event.headers["user-agent"]);
 
-    const body = qs.parse(event.body);
-    const options = body.options || {};
+        const body = qs.parse(event.body);
+        const options = body.options || {};
 
-    staticman
-        .processEntry(body.fields, options)
-        .then(() => {
-            callback(null, {
-                statusCode: 200,
-                body: "Success",
+        staticman
+            .processEntry(body.fields, options)
+            .then(() => {
+                callback(null, {
+                    statusCode: 200,
+                    body: "Success",
+                });
+            })
+            .catch((error) => {
+                callback(error);
             });
-        })
-        .catch((error) => {
-            callback(error);
-        });
+    });
 };
